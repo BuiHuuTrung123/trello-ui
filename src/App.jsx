3299da094350970d22b69109d50d0af92139111b
TrungBui12345 <trungdoc345678@gmail.com>
2025-09-01 09:32:37 +0700

    refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,12 @@
-
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
-import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-import {
-  Experimental_CssVarsProvider as CssVarsProvider,
-  experimental_extendTheme as extendTheme,
-  useColorScheme,
-} from '@mui/material/styles';
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme();
-
-  const handleChange = (event) => {
-    const selectedMode = event.target.value
-    setMode(selectedMode)
-    //setAge(event.target.value);
-  };
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-
-        <MenuItem value="light">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '7px' }}><LightModeIcon fontSize='small' />Light</div>
-        </MenuItem>
-        <MenuItem value="dark">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '7px' }}><DarkModeIcon fontSize='small' />Dark</div>
-        </MenuItem>
-        <MenuItem value="system">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '7px' }}><SettingsBrightnessIcon fontSize='small' />System</div>
-
-        </MenuItem>
-      </Select>
-    </FormControl>
-  );
-}
+import Board from './pages/Boards/_id'
 
 function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box sx={{
-        backgroundColor: 'primary.contrastText',
-        width: '100%',
-        height: (theme) => theme.trelloCustom.appBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        <ModeSelect />
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: '100%',
-        height: (theme) => theme.trelloCustom.boardBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        board bar
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        height: (theme) => `calc(100vh - ${theme.trelloCustom.appBarHeight} - ${theme.trelloCustom.boardBarHeight})`,
-
-      }}>Board content</Box>
-    </Container>
+  <>
+  {/* {React router DOM} */}
+  {/* {BoardDetail} */}
+  <Board/>
+  </> 
   )
 }
 
